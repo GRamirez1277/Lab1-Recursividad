78405ed3088332e41d449e3ad742ba287f812486
GRamirez1277 <94391774+GRamirez1277@users.noreply.github.com>
2024-01-19 13:42:40 -0600

    Add files via upload

--- Recusividad/src/recusividad/PalindromoAir.java
+++ Recusividad/src/recusividad/PalindromoAir.java
@@ -1,12 +1,26 @@
-package recusividad;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package     labrecursividad;
 
+/**
+ *
+ * @author Azalia
+ */
 public class PalindromoAir {
-     
+    
     Ticket[] ticket;
+    public int posicion;
+    public double totalIngresos;
+    public double totalPago;
+    
     
     public PalindromoAir(){
         ticket=new Ticket[30];
     }
+    
+    //aFirst Available
     
     public int firstAvailable(){
         return firstAvailable(0);
@@ -27,6 +41,7 @@ public class PalindromoAir {
         
     }
     
+    //b)Buscar Pasajero
     public int buscarPasajeros(String nombrePasajero){
        return buscarPasajero(nombrePasajero,0) ;
     }
@@ -43,6 +58,7 @@ public class PalindromoAir {
         }
     }
     
+    //c)Es Palindroma
     public static boolean isPalindromo(String nombrePasajero){
        return isPalindromo(nombrePasajero,0,nombrePasajero.length()-1);
     }
@@ -56,6 +72,7 @@ public class PalindromoAir {
         return true;
       }
     
+   //d)Print Passengers
     public void printPassengers(){
        printPassengers(0);
     }
@@ -69,4 +86,75 @@ public class PalindromoAir {
          printPassengers(posicion+1);
     }
     
-}
+    //e) Income
+    public double income(){
+        return income(totalIngresos,0);
+    }
+    private double income(double totalIngresos,int posicion){
+       if(posicion>=ticket.length){
+            return totalIngresos;
+        } 
+       if(ticket[posicion]!=null){
+           totalIngresos+= ticket[posicion].getTotalPagado();
+           return income(totalIngresos,posicion+1);
+        }
+        return income(totalIngresos,posicion+1);
+    }
+    
+    //g)Sell Ticket
+    
+    public void sellTicket(String nombrePasajero){
+       
+        totalPago=0;
+        posicion = firstAvailable(); 
+        if(posicion!=-1){
+            if(isPalindromo(nombrePasajero)==false){
+                totalPago=800;
+            }
+            else{
+                totalPago=800*0.8;
+            }
+            ticket[firstAvailable()] = new Ticket(nombrePasajero, totalPago);
+            
+            System.out.println("--------Monto a Pagar---------"+"\nNombre: "+ticket[posicion].getNombrePasajero()+
+                    "\nMonto a pagar: Lps. "+ticket[posicion].getTotalPagado());      
+        }   
+    }
+    
+    //f)Reset
+    public void reset(){
+        totalIngresos = 0;
+        reset(0);
+    }
+    private void reset(int posicion){
+         if(posicion>=ticket.length){  
+             return;
+        }
+         ticket[posicion]=null;
+          reset(posicion+1);      
+    }
+    
+    
+    
+    //h)Cancel Ticket
+     public boolean cancelTicket(String nombrePasajero){
+        
+         if (buscarPasajero(nombrePasajero,0)!=-1){
+         ticket [posicion]=null;
+     }else{
+             return false;
+         }
+         return true;
+     }
+    
+    //i) dispatch
+     public void dispatch(){
+         
+         System.out.println("Ingreso generado: "+income());
+         reset(0);
+         totalIngresos=0;
+     }
+    }
+
+    
+
--- Recusividad/src/recusividad/Ticket.java
+++ Recusividad/src/recusividad/Ticket.java
@@ -1,21 +1,39 @@
-package recusividad;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labrecursividad;
 
 public class Ticket {
-    private String nombrePasajero;
-    private double totalTicket;
-
-    public Ticket(String nombrePasajero, double totalTicket) {
-        this.nombrePasajero = nombrePasajero;
-        this.totalTicket = totalTicket;
-    }
-
-    public String getNombrePasajero() {
-        return nombrePasajero;
-    }
-
-    public double getTotalTicket() {
-        return totalTicket;
-    }
-    
+    //Atributos 
+   public String nombrePasajero;
+   public double totalPagado;
+   
+   public Ticket(String nombrePasajero,double totalPagado){
+       this.nombrePasajero=nombrePasajero;
+       this.totalPagado=totalPagado;
+   }
+   
+    //Set y get
+   public void setNombrePaasajero(String nombrePasajero){
+       this.nombrePasajero=nombrePasajero;
+   }
+   
+   public String getNombrePasajero(){
+       return nombrePasajero;
+   }
+   
+   public void setTotalPagado(double totalPagado){
+       this.totalPagado=totalPagado;
+   }
+   
+   public double getTotalPagado(){
+       return totalPagado;
+   }
+   
+   public void print(){
+       System.out.println("-------Datos de Ticket-------"+"\nNombre del Pasajero: "+nombrePasajero+
+       "\nTotal Pagado Ticket: Lps. "+totalPagado);
+   }
     
 }
